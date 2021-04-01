64d46523c4bceca1b582effefe2c93d25daf5c8b
letsandeepio <sandeepchopra7@gmail.com>
2021-04-01 11:34:18 -0400

    remove unused style defaults

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCEHzrKGg2ZlHA8b7dgluEOfzzymTg223Q',
+  authDomain: 'whatsapp2-22709.firebaseapp.com',
+  projectId: 'whatsapp2-22709',
+  storageBucket: 'whatsapp2-22709.appspot.com',
+  messagingSenderId: '210904271102',
+  appId: '1:210904271102:web:17855398a404d87e233daf'
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore;
+const auth = app.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
