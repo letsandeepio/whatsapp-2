62c07d7057cbffba88a9c1b2d0fc5e109b1e084f
letsandeepio <sandeepchopra7@gmail.com>
2021-04-04 18:46:20 -0400

    style the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,28 @@
 import Head from 'next/head';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whitesmoke;
+`;
+const LoginContainer = styled.div`
+  padding: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
+`;
+
+const Logo = styled.img`
+  height: 200px;
+  width: 200px;
+  margin-bottom: 50px;
+`;
 
 const Login = () => {
   return (
@@ -9,7 +30,10 @@ const Login = () => {
       <Head>
         <title>Login</title>
       </Head>
-      <h1>Hello</h1>
+      <LoginContainer>
+        <Logo src="https://i.pinimg.com/originals/c4/2c/93/c42c9352e2696ce633fc34ae711c2c68.png" />
+        <Button variant="outlined">Sign in with Google</Button>
+      </LoginContainer>
     </Container>
   );
 };
