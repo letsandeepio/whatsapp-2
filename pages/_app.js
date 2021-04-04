d984c2fafb664886aaa4e3ba0535f1547200a37b
letsandeepio <sandeepchopra7@gmail.com>
2021-04-04 17:55:46 -0400

    adds login component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import '../styles/globals.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db } from './firebase';
+import '../styles/globals.css';
+import { auth } from '../firebase';
+import Login from './login';
 
 function MyApp({ Component, pageProps }) {
   const [user] = useAuthState(auth);
+  if (!user) return <Login />;
   return <Component {...pageProps} />;
 }
 
---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const Login = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Hello</h1>
+    </Container>
+  );
+};
+
+export default Login;
