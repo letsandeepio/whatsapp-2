5c741bd852a12a7b3fec655c51c4405b410485fa
letsandeepio <sandeepchopra7@gmail.com>
2021-04-05 12:43:24 -0400

    add server side rendering for Chat page

---
+++ components/ChatScreen.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatScreen = () => {
+  return <Container>Hello </Container>;
+};
+
+export default ChatScreen;
--- pages/chat/[id].tsx
+++ pages/chat/[id].tsx
@@ -1,22 +1,66 @@
 import Head from 'next/head';
 import styled from 'styled-components';
+import ChatScreen from '../../components/ChatScreen';
 import Sidebar from '../../components/Sidebar';
+import { db } from '../../firebase';
 
 const Container = styled.div`
   display: flex;
 `;
-const ChatContainer = styled.div``;
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  --ms-overflow-style: none;
+  scrollbar-width: none;
+`;
 
-const Chat = () => {
+const Chat = ({ chat, messages }) => {
   return (
     <Container>
       <Head>
         <title>Chat</title>
       </Head>
       <Sidebar />
-      <ChatContainer></ChatContainer>
+      <ChatContainer>
+        <ChatScreen />
+      </ChatContainer>
     </Container>
   );
+};
+
+export const getServerSideProps = async (context) => {
+  const ref = db.collection('users').doc(context.query.id);
+  const messagesRes = await ref
+    .collection('messages')
+    .orderBy('timestamp', 'asc')
+    .get();
+
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }))
+    .map((message) => ({
+      ...message,
+      timestamp: message.timestamp.toDate().getTime()
+    }));
+
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data()
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat
+    }
+  };
 };
 
 export default Chat;
